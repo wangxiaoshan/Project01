feb301e3e766e577c7cc3396027d2641f61b9d77
wangxiaoshan <wxjwxpwxs@163.com>
2019-09-29 11:04:16 +0800

    bendiaaaaa

--- Module01/src/com/dzsw/bean/Customer.java
+++ Module01/src/com/dzsw/bean/Customer.java
@@ -3,6 +3,7 @@ package com.dzsw.bean;
 public class Customer {
     public static  void  main(String[] args){
         System.out.println("Customer!!!");
+        System.out.println("测试提交");
     }
     public void test1(){
         System.out.println("11");
